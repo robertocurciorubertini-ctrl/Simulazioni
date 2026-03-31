079107e7c2e36b81d177aaa6e4c3e25ccf9e7859
robertocurciorubertini-ctrl <robertocurciorubertini@gmail.com>
2026-03-31 11:56:34 +0200

    Add files via upload

--- build_catalog.py
+++ build_catalog.py
@@ -84,7 +84,7 @@ def parse_args() -> argparse.Namespace:
 
 def find_html_files(root: Path) -> list[Path]:
     html_files = []
-    excluded_dirs = {"theory", "nome_cartella"}
+    excluded_dirs = {"theory", "HWIP"}
 
     for p in root.rglob("*"):
         if not p.is_file():
